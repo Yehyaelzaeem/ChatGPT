f0607400395b05cbbf7353d8204477cdb460ac09
Yahia <Yehyaelzaeem>
2023-12-11 23:12:35 +0000

    -add permission internet

--- lib/core/global/constatnties/app_string_constant.dart
+++ lib/core/global/constatnties/app_string_constant.dart
@@ -1,7 +1,7 @@
 class AppStrings{
   static const String onBoardingTitle='Welcome to\n ChatGPT';
   static const String onBoardingSubTitle='Ask anything, get your answer';
-  static const String apiToken='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';
+  static const String apiToken='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';
   static const String baseUrl ='https://api.openai.com/v1';
   static const String currentModel ='text-babbage-001';
 
