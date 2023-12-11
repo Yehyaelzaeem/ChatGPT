57cf72caa5f2930c2172c03132b0453517981d3c
Yahia <Yehyaelzaeem>
2023-12-11 23:50:18 +0000

    update api && apk

--- lib/core/global/constatnties/app_string_constant.dart
+++ lib/core/global/constatnties/app_string_constant.dart
@@ -1,8 +1,9 @@
 class AppStrings{
   static const String onBoardingTitle='Welcome to\n ChatGPT';
   static const String onBoardingSubTitle='Ask anything, get your answer';
-  static const String apiToken='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';
+  static const String apiToken='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';
   static const String baseUrl ='https://api.openai.com/v1';
-  static const String currentModel ='text-babbage-001';
+  static const String currentModel ='gpt-3.5-turbo';
+  static const String currentModel2 ='text-babbage-001';
 
 }
